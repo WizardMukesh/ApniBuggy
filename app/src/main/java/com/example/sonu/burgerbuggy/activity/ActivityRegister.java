f8784d7640f56376dfc52e26ffbe91f4c866b167
aahul <rhlkumawat7@gmail.com>
2017-09-21 12:50:07 +0530

    rk

---
+++ app/src/main/java/com/example/sonu/burgerbuggy/activity/ActivityRegister.java
@@ -0,0 +1,36 @@
+package com.example.sonu.burgerbuggy.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.sonu.burgerbuggy.R;
+
+/**
+ * Created by Nishant on 9/21/2017.
+ */
+
+public class ActivityRegister extends AppCompatActivity {
+    private TextView btnAlreadyAc;
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        findViewsByID();
+        btnAlreadyAc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(ActivityRegister.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+    public void findViewsByID(){
+        btnAlreadyAc = (TextView)findViewById(R.id.btn_already_have_ac);
+    }
+}
--- app/src/main/java/com/example/sonu/burgerbuggy/activity/LoginActivity.java
+++ app/src/main/java/com/example/sonu/burgerbuggy/activity/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.sonu.burgerbuggy.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.sonu.burgerbuggy.R;
 import com.example.sonu.burgerbuggy.adapter.CategoryAdapter;
@@ -11,11 +16,39 @@ import com.example.sonu.burgerbuggy.model.Category;
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText editEmail,editPassword;
+    TextView btnRegister,btnForgetPassword;
+    private Button btnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        findViewsByID();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(LoginActivity.this,ActivityRegister.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+    public void findViewsByID(){
+        editEmail = (EditText)findViewById(R.id.edit_email);
+        editPassword = (EditText)findViewById(R.id.edit_password);
+        btnRegister = (TextView)findViewById(R.id.btn_register_account);
+        btnForgetPassword = (TextView)findViewById(R.id.btn_forget_password);
+        btnLogIn = (Button)findViewById(R.id.btn_login);
+    }
+
 }
